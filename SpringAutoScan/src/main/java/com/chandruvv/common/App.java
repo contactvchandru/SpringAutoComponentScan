fdc2befec68622bcc9e1c45e949bac0df81cf1e9
contactvchandru <contactvchandru@gmail.com>
2014-01-08 16:08:03 +0530

    This involves Spring Framewokr Scanning for Beans with @Component Annotations Automatically using context-scan

---
+++ SpringAutoScan/src/main/java/com/chandruvv/common/App.java
@@ -0,0 +1,29 @@
+package com.chandruvv.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+    	 
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+        
+        HelloWorld obj1 = (HelloWorld) context.getBean("conbean");
+        obj1.printHello();
+
+        HelloWorld obj2 = (HelloWorld) context.getBean("conbean1");
+        obj2.printHello();
+        
+        Scan obj3=(Scan)context.getBean("scan");
+        System.out.println(obj3);
+    }
+}
---
+++ SpringAutoScan/src/main/java/com/chandruvv/common/ScanImpl.java
@@ -0,0 +1,14 @@
+package com.chandruvv.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScanImpl {
+	
+	public String toString()
+	{
+		return "--->Hi this is ScanImpl";
+		
+	}
+
+}
